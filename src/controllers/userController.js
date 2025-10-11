6310ccf8bb9efb721dcc646f1db2f4b3b55dbf15
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-10-11 01:06:49 +0100

    Add endpoints to get current user resources

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,35 @@
 import prisma from "../lib/prisma.js";
 import bcrypt from "bcryptjs";
 
+export const getCurrentUserProfile = async (req, res) => {
+  const { id } = req.user;
+
+  const user = await prisma.user.findUnique({
+    where: { id },
+    omit: { password: true },
+  });
+
+  res.json(user);
+};
+
+export const listCurrentUserPosts = async (req, res) => {
+  const { id } = req.user;
+
+  const posts = await prisma.post.findMany({
+    where: {
+      authorId: id,
+    },
+    omit: {
+      authorId: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  res.json(posts);
+};
+
 export const createUser = async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { createUser } from "../controllers/userController.js";
+import {
+  createUser,
+  getCurrentUserProfile,
+  listCurrentUserPosts,
+} from "../controllers/userController.js";
 import { validateUser } from "../lib/validators.js";
+import { requireAuth } from "../lib/auth.js";
 
 const userRouter = Router();
 
 userRouter.post("/", validateUser, createUser);
+
+userRouter.get("/me", requireAuth, getCurrentUserProfile);
+
+userRouter.get("/me/posts", requireAuth, listCurrentUserPosts);
 
 export default userRouter;
