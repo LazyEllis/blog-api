a11b40c484a927f995727eafb9e4ac758c784b79
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-10-30 22:47:11 +0100

    Send a generic error for failed authentication
    A generic error message is now used when authenticating a user in order
    to reveal existence of e-mails in the database.

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -17,12 +17,12 @@ passport.use(
     try {
       const user = await prisma.user.findUnique({ where: { email } });
       if (!user) {
-        return done(null, false, { message: "Incorrect email." });
+        return done(null, false, { message: "Incorrect email or password" });
       }
 
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
-        return done(null, false, { message: "Incorrect password." });
+        return done(null, false, { message: "Incorrect email or password" });
       }
 
       return done(null, user);
