5efdc531766c894ed2e341d39d34682c71ebee6c
Emmanuel Danison <117293986+LazyEllis@users.noreply.github.com>
2025-10-30 22:53:48 +0100

    Add route to authenticate admins

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -20,3 +20,27 @@ export const generateToken = (req, res, next) => {
     res.json({ token });
   })(req, res, next);
 };
+
+export const generateAdminToken = (req, res, next) => {
+  passport.authenticate("local", { session: false }, (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.status(401).json({ message: info.message });
+    }
+
+    if (!user.isAdmin) {
+      return res.status(401).json({ message: "Incorrect email or password" });
+    }
+
+    const token = jwt.sign(
+      { sub: user.id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" },
+    );
+
+    res.json({ token });
+  })(req, res, next);
+};
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { generateToken } from "../controllers/authController.js";
+import {
+  generateAdminToken,
+  generateToken,
+} from "../controllers/authController.js";
 import { sanitizeEmail } from "../lib/validators.js";
 
 const authRouter = Router();
 
 authRouter.post("/token", sanitizeEmail, generateToken);
+
+authRouter.post("/admin/token", sanitizeEmail, generateAdminToken);
 
 export default authRouter;
